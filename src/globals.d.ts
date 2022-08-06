eb6f003836afe5fe13d29f94523276077729e353
Maslomeister <maslomeister@gmail.com>
2022-08-06 18:27:24 +0300

    refactor: new types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,22 +1,68 @@
-interface ITextAreaSize {
+interface ISize {
 	width: number;
 	height: number;
 }
 
-interface ICanvas {
-	width: number;
-	height: number;
-	image: string;
-}
+type ITextAreaSize = ISize;
 
-interface IMatrixToImage {
+interface ICurrentKey {
+	keycode: string;
 	x: number;
 	y: number;
-	keyMatrix: string;
+}
+
+interface IKeyboardImage extends ISize {
+	src: string;
+}
+
+// interface ICanvas {
+// 	width: number;
+// 	height: number;
+// 	image: string;
+// }
+
+interface IMatrixImageMapping {
+	x: number;
+	y: number;
+	keycode: string;
 	[index: number]: [number, number];
 }
 
+// interface IMatrixToImage {
+// 	x: number;
+// 	y: number;
+// 	keyMatrix: string;
+// 	[index: number]: [number, number];
+// }
+
 interface IConfig {
-	matrixToImage: IMatrixToImage[];
-	image: ICanvas;
+	matrixImageMapping: IMatrixImageMapping[];
+	keyboardImage: IKeyboardImage;
+}
+
+interface IDataPoints {
+	x: number;
+	y: number;
+	value: number;
+}
+
+interface IHeatMapData {
+	maxKeyPresses: number;
+	dataPoints: IDataPoints[];
+}
+
+interface IHeatmapConfig {
+	container?: HTMLElement;
+	canvas?: HTMLCanvasElement;
+	shadowCanvas?: HTMLCanvasElement;
+	radius?: number;
+	width?: number;
+	height?: number;
+	gradient?: Record<number, string>;
+	blur?: number;
+	backgroundColor?: string;
+	opacity?: number;
+	maxOpacity?: number;
+	minOpacity?: number;
+	useGradientOpacity?: boolean;
 }
