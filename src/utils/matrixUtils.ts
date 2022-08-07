e3e66157b182dac5833d2c4fddebcaeaa40347b3
Maslomeister <maslomeister@gmail.com>
2022-08-07 08:43:52 +0300

    refactor: cleanup

--- src/utils/matrixUtils.ts
+++ src/utils/matrixUtils.ts
@@ -5,7 +5,6 @@ const COL_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijilmnopqrstuvwxyz";
 
 const MAX_ROW = 52;
 const MAX_COL = 52;
-// let hasKeyCodes = true;
 
 const helperStartPattern = new RegExp(`┌──┐`, "g");
 const helperEndPattern = new RegExp(`└──┘`, "g");
@@ -31,66 +30,29 @@ export const getMatrixKeys = (matrix: string) => {
 export const processCsv = (
 	data: (string | number)[][],
 	matrixImageMapping: IMatrixImageMapping[]
-): Record<number, IHeatMapData> => {
+): IHeatMapData[] => {
 	let layersAmount = 0;
 	const keyFreq: Record<string, number[]> = {};
-	// const keyCode = data[0][0];
-	// if (typeof keyCode !== "string") hasKeyCodes = false;
-
-	// if (hasKeyCodes) {
-	// 	for (let i = 1; i < data.length; i++) {
-	// 		const keycode = data[i][0];
-	// 		const row = data[i][1] as number;
-	// 		const col = data[i][2] as number;
-	// 		const layer = data[i][3] as number;
-	// 		// console.log(layer);
-	// 		let rowcol = "";
-
-	// 		if (row < MAX_ROW && col < MAX_COL) {
-	// 			rowcol = ROW_LETTERS[row] + COL_LETTERS[col];
-	// 		} else {
-	// 			// Is probably a combo whose row, col = 254, 254
-	// 			rowcol = "ØØ"; // 'Ø' is totally arbitrary
-	// 		}
-
-	// 		// keyFreq[rowcol] = [0];
-	// 		if (keyFreq[rowcol] === undefined) keyFreq[rowcol] = [];
-	// 		if (layer !== undefined) {
-	// 			keyFreq[rowcol][layer] = (keyFreq[rowcol][layer] || 0) + 1;
-	// 		}
-
-	// 		// keyFreq[rowcol][layer] = (keyFreq[rowcol][layer] || 0) + 1;
-	// 	}
-	// 	console.log(keyFreq);
-	// } else {
-	// 	console.log("no keycodes");
-	// }
 
 	for (let i = 1; i < data.length; i++) {
-		// const keycode = data[i][0];
 		const row = data[i][1] as number;
 		const col = data[i][2] as number;
 		const layer = data[i][3] as number;
 		if (layer > layersAmount) layersAmount = layer;
-		// console.log(layer);
+
 		let rowcol = "";
 
 		if (row < MAX_ROW && col < MAX_COL) {
 			rowcol = ROW_LETTERS[row] + COL_LETTERS[col];
 		} else {
-			// Is probably a combo whose row, col = 254, 254
 			rowcol = "ØØ"; // 'Ø' is totally arbitrary
 		}
 
-		// keyFreq[rowcol] = [0];
 		if (keyFreq[rowcol] === undefined) keyFreq[rowcol] = [];
 		if (layer !== undefined) {
 			keyFreq[rowcol][layer] = (keyFreq[rowcol][layer] || 0) + 1;
 		}
-
-		// keyFreq[rowcol][layer] = (keyFreq[rowcol][layer] || 0) + 1;
 	}
-	// console.log(keyFreq);
 
 	const layerTotalKeyPresses = [];
 
@@ -110,23 +72,10 @@ export const processCsv = (
 		)
 	);
 
-	// console.log(layerTotalKeyPresses);
-
-	// console.log(matrixToImage);
 	const rowCols = matrixImageMapping.map((item) => item.keycode);
-	// console.log(keyFreq);
-	// console.log(rowCols);
-	// (Object.keys(matrixToImage) as (keyof typeof matrixToImage)[]).find((key) => {
-	// 	return matrixToImage[key] === "keyMatrix";
-	// });
-	// console.log(rowCols);
 
-	// const heatMapData: Record<number, number[]> = {};
+	const heatmapData: IHeatMapData[] = [];
 
-	const heatmapData: Record<number, { x: number; y: number; value: number }[]> =
-		[];
-
-	const rowcols = Object.keys(rowCols);
 	let i = rowCols.length;
 	while (i--) {
 		const rowcol = rowCols[i];
@@ -144,46 +93,39 @@ export const processCsv = (
 			const { x, y } = matrixImageMapping.filter(
 				(item) => item.keycode === rowcol
 			)[0];
-			if (heatmapData[j] === undefined) heatmapData[j] = [];
 
-			heatmapData[j].push({ x, y, value });
+			if (heatmapData[j] === undefined)
+				heatmapData[j] = {
+					maxKeyPresses: 0,
+					dataPoints: [],
+				};
+
+			heatmapData[j].dataPoints.push({ x, y, value });
 		}
-		// console.log(data);
 
-		// console.log(i);
-		// console.log(coordsAndValue);
-		// console.log(coordsAndValue);
-		// coordsAndValue = totalKeypressesCount;
-		// if (heatMapData[layersAmount + 1] == undefined) {
-		// 	heatMapData[layersAmount + 1] = [];
-		// }
-		// heatMapData[layersAmount + 1].push(coordsAndValue);
-	}
-	const resultData: Record<number, IHeatMapData> = [];
-	for (let i = 0; i <= layersAmount; i++) {
-		if (resultData[i] == undefined) {
-			resultData[i] = {
-				maxKeyPresses: layerTotalKeyPresses[i],
-				dataPoints: heatmapData[i],
+		if (heatmapData[layersAmount + 1] === undefined)
+			heatmapData[layersAmount + 1] = {
+				maxKeyPresses: 0,
+				dataPoints: [],
 			};
-		}
+
+		const { x, y } = matrixImageMapping.filter(
+			(item) => item.keycode === rowcol
+		)[0];
+
+		heatmapData[layersAmount + 1].dataPoints.push({
+			x,
+			y,
+			value: reduceSum(keyFreq[rowcol]),
+		});
 	}
-	const keys: number[] = Object.keys(resultData).map(Number);
-	const allKeyPresses = keys.reduce((prevVal, currentValue, index) => {
-		return (prevVal += resultData[index].maxKeyPresses);
-	}, 0);
-	console.log(allKeyPresses);
-	// const allLayersPresses = reduceSum(keyFreq[rowcols]);
-	// // console.log(keyFreq[rowcol]);
-	// const coordsAndValue = { ...keyFreq[rowcols] };
-	// if (resultData[layersAmount + 1] == undefined) {
-	// 	resultData[layersAmount + 1] = {
-	// 		maxKeyPresses: allLayersPresses,
-	// 		dataPoints: coordsAndValue,
-	// 	};
-	// }
 
-	// console.log(resultData);
+	heatmapData.map(
+		(item) =>
+			(item.maxKeyPresses = Math.max(
+				...item.dataPoints.map((dataPoint) => dataPoint.value)
+			))
+	);
 
-	return resultData;
+	return heatmapData;
 };
