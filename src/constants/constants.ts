0ba85c37dce5ace12b864c30da2531cc1f76e491
Maslomeister <maslomeister@gmail.com>
2022-08-07 09:47:55 +0300

    update

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,5 @@
 export const MAX_IMAGE_HEIGHT = 1550;
 export const MAX_IMAGE_WIDTH = 1550;
+export const DEFAULT_RADIUS = 50;
+export const DEFAULT_MAX_OPACITY = 255;
+export const DEFAULT_OPACITY = 1;
