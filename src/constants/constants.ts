b067663521acd6c0cd258e8ed0a1dbc6889f0595
Maslomeister <maslomeister@gmail.com>
2022-08-08 06:32:50 +0300

    feat: new constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,9 @@ export const MAX_IMAGE_HEIGHT = 1550;
 export const MAX_IMAGE_WIDTH = 1550;
 export const DEFAULT_RADIUS = 50;
 export const DEFAULT_MAX_OPACITY = 255;
+export const DEFAULT_CURRENT_LAYER = 0;
 export const DEFAULT_OPACITY = 1;
+export const DEFAULT_GRADIENT = [
+	{ id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d", value: 0, color: "#f73791" },
+	{ id: "9b1ddf4d-3b7d-4bad-9bdd-2sdfeserwb6d", value: 1, color: "#391360" },
+];
